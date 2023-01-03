8184b9f1ad6c85342cc9b7553c63f309972c3acb
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2023-01-03 16:06:19 +0100

    Initialize monthly invoice.

--- BuilderDesignPatterExample/Program.cs
+++ BuilderDesignPatterExample/Program.cs
@@ -1,2 +1,9 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using BuilderDesignPatterExample.Controllers.DirectorControllers;
+using BuilderDesignPatterExample.Controllers.InvoiceBuilderControlers;
+
+var builder = new InvoiceBuilder();
+var director = new Director(builder);
+
+var monthlyInvoice = director.CreateMonthlyInvoice();
+
+Console.WriteLine(monthlyInvoice.Date);
