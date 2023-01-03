4315501992985186c9bd1c148f2bca7b9d0f955e
Piotr Szymala <piotr.szymala@edu.uekat.pl>
2023-01-03 13:32:35 +0100

    Fill Invoice class.

--- BuilderDesignPatterExample/Model/Invoice.cs
+++ BuilderDesignPatterExample/Model/Invoice.cs
@@ -2,5 +2,10 @@ namespace BuilderDesignPatterExample.Model;
 
 public class Invoice
 {
-    
+    public string Number { get; set; }
+    public DateTime Date { get; set; }
+    public string Vendor { get; set; }
+    public string Vendee { get; set; }
+    public IEnumerable<string> LineItems { get; set; }
+    public string Note { get; set; }
 }
